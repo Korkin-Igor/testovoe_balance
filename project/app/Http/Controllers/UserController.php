4bc3532848ddad1b5f079df6424d310a96121ef1
Korkin-Igor <korkinigorttit@gmail.com>
2025-10-28 15:03:50 +0700

    start writing auth (controller + routes)

--- project/app/Http/Controllers/UserController.php
+++ project/app/Http/Controllers/UserController.php
@@ -4,6 +4,21 @@ namespace App\Http\Controllers;
 
 class UserController extends Controller
 {
+    public function register()
+    {
+        //
+    }
+
+    public function login()
+    {
+        //
+    }
+
+    public function logout()
+    {
+        //
+    }
+
     public function getBalance()
     {
         //
--- project/routes/api.php
+++ project/routes/api.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/deposit', [PaymentController::class, 'deposit']);
     Route::post('/withdraw', [PaymentController::class, 'withdraw']);
@@ -11,4 +14,6 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/balance/{user}', [UserController::class, 'getBalance'])
         ->where('user', '[0-9]+');
+
+    Route::get('/logout', [UserController::class, 'logout']);
 });
