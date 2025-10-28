47150e6c3dfc23126abc43ac86e65daa78134e1a
Korkin-Igor <korkinigorttit@gmail.com>
2025-10-28 14:52:09 +0700

    create routes + make controllers

---
+++ project/app/Http/Controllers/PaymentController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PaymentController extends Controller
+{
+    public function deposit()
+    {
+        //
+    }
+
+    public function withdraw()
+    {
+        //
+    }
+
+    public function transfer()
+    {
+        //
+    }
+}
--- project/routes/api.php
+++ project/routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/deposit', [PaymentController::class, 'deposit']);
+    Route::post('/withdraw', [PaymentController::class, 'withdraw']);
+    Route::post('/transfer', [PaymentController::class, 'transfer']);
+
+    Route::get('/balance/{user}', [UserController::class, 'getBalance'])
+        ->where('user', '[0-9]+');
+});
