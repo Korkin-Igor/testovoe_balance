ce02c956ade4535e92f9ff7ed3524501d8db2be2
Korkin-Igor <korkinigorttit@gmail.com>
2025-10-28 14:47:10 +0700

    create models

---
+++ project/app/Models/Payment.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Payment extends Model
+{
+    protected $fillable = [
+        'from_user_id',
+        'to_user_id',
+        'amount',
+        'comment',
+    ];
+
+    public $timestamps = false;
+
+    public function from_user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'from_user_id');
+    }
+
+    public function to_user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'to_user_id');
+    }
+}
